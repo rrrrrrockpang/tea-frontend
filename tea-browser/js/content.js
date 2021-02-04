a4808b412f2355171699b9b61c66088f9e73f2f9
rrrrrrockpang <ypang2@cs.washington.edu>
2021-02-04 01:59:22 -0800

    select clicked area on aspredicted

--- tea-browser/js/content.js
+++ tea-browser/js/content.js
@@ -1,6 +1,7 @@
-$("[name='text2']").on("click", () => {
-    // $("#counter2")
-    console.log("Hi");
+$("[name='text2']").on("click", (event) => {
+
     // console.log($(".center1 .row.row_create .col-xs-12 .row.row_create:nth-of-type(4)"))
-    $(".center1 .row_create .col-xs-12 .row_create:nth-of-type(5)").remove()
+//    $(".center1 .row_create .col-xs-12 .row_create:nth-of-type(5)").remove()
+    console.log($(event.target).parent())
+    $(event.target).parent().css("background-color", "red");
 })
