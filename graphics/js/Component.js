5b27712a118f1ebc4617eae90c8f8d97311aff35
Rock Pang <ypang2@cs.washington.edu>
2021-02-18 22:08:45 -0800

    refactor initial try

--- graphics/js/Component.js
+++ graphics/js/Component.js
@@ -15,56 +15,14 @@ class Component {
     }
 
     dotInPlayground(svg, x, y) {
-        var tooltip = d3.select("#playground")
-            .append("div")
-            .style("opacity", 0)
-            .attr("class", "tooltip")
-            .style("background-color", "black")
-            .style("border-radius", "5px")
-            .style("padding", "10px")
-            .style("color", "white")
-
-
-        var showTooltip = function(d) {
-            tooltip
-                .transition()
-                .duration(200)
-            tooltip
-                .style("opacity", 1)
-                .html("hi")
-                .style("left", (d3.mouse(this)[0]+30) + "px")
-                .style("top", (d3.mouse(this)[1]+30) + "px")
-        }
-        var moveTooltip = function(d) {
-            tooltip
-                .style("left", (d3.mouse(this)[0]+30) + "px")
-                .style("top", (d3.mouse(this)[1]+30) + "px")
-        }
-        var hideTooltip = function(d) {
-            tooltip
-                .transition()
-                .duration(200)
-                .style("opacity", 0)
-        }
         svg.append("circle")
             .attr("class", "bubbles")
             .attr("cx", x)
             .attr("cy", y)
             .attr("r", 100 )
             .style("fill", "green")
-            // -3- Trigger the functions
-            .on("mouseover", showTooltip )
-            .on("mousemove", moveTooltip )
-            .on("mouseleave", hideTooltip )
-
-        // svg.append('circle')
-        //     .attr('cx', x)
-        //     .attr('cy', y)
-        //     .attr('r', 50)
-        //     .attr('stroke', 'black')
-        //     .attr('fill', '#69a3b2')
-        //     .on("mouseover", showTooltip)
-        //     .on("mousemove", moveTooltip)
-        //     .on("hideTooltip", hideTooltip)
+            .on("mouseover", showTooltip)
+            .on("mousemove", moveTooltip)
+            .on("mouseleave", hideTooltip)
     }
 }
