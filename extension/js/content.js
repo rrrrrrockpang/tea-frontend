cbeaa1ccfe001362034ce82bea70b006f102e82c
rrrrrrockpang <ypang2@cs.washington.edu>
2021-03-25 16:31:41 -0700

    add description

--- extension/js/content.js
+++ extension/js/content.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     // handle variable listener
     // handleVariableListeners();
 
+    addInstruction();
+
     // add plugin sections
     addAnalysisPreregistea();
     addConstructPreregistea();
@@ -15,3 +17,45 @@ $(document).ready(function() {
     body.append(addTeaFloatingBtn());
     body.append(addMethodFloatingBtn());
 });
+
+const STEP_1_DESCRIPTION = "Step 1: Define your study in Preregistea";
+const STEP_2_DESCRIPTION = "Step 2: Adjust the automatically-generated preregistration";
+const STEP_3_DESCRIPTION = "Step 3: Preview analysis code, “method” section, and Preregistration";
+const STEP_4_DESCRIPTION = "Step 4: Submit Preregistration and Push your Analysis Code to Github";
+
+const addInstruction = () => {
+    const blueAlert = $(".center1 > .alert-info");
+
+    const description = $("<div class='.row .row_create'></div>").css({
+        display: "flex",
+        "flex-direction": "row",
+        "justify-content": "space-between"
+    });
+
+    const first_box = $(`<div id='first_box' class='description-box'><p>${STEP_1_DESCRIPTION}</p></div>`);
+    const second_box = $(`<div id='second_box' class='description-box'>${STEP_2_DESCRIPTION}</div>`);
+    const third_box = $(`<div id='third_box' class='description-box'>${STEP_3_DESCRIPTION}</div>`);
+    // const fourth_box = $(`<div id='fourth_box' class='description-box'>${STEP_4_DESCRIPTION}</div>`);
+
+    description.append([first_box, second_box, third_box]);
+
+    description.insertAfter(blueAlert);
+
+    new LeaderLine(
+      document.getElementById('first_box'),
+      document.getElementById('second_box'),
+        {color: 'black', size: 3}
+    );
+
+    new LeaderLine(
+      document.getElementById('second_box'),
+      document.getElementById('third_box'),
+                {color: 'black', size: 3}
+
+    );
+
+    // new LeaderLine(
+    //   document.getElementById('third_box'),
+    //   document.getElementById('fourth_box')
+    // );
+}
